e0e43acc851c4c0ab076c1f8cbbefc365ded48bd
gulssumm <gulsumcetinozlu@gmail.com>
2024-08-31 20:25:46 +0300

    6-design-main-page-buttons??

--- main.py
+++ main.py
@@ -14,9 +14,6 @@ screen_height = 563
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Balloon Shooting Game")
 
-# Display the main menu
-main_menu(screen)  # Call the main menu function
-
 # Load the background image
 background = pygame.image.load("C:/Users/21180/PycharmProjects/balloon_game/images/background.jpg").convert()
 
@@ -26,11 +23,30 @@ pygame.time.set_timer(SPAWN_BALLOON, 500)
 # Instantiate Shooter
 shooter = Shooter(screen_width, screen_height)
 
-# Default score
+# Default values
+level = 0
 score = 0
 
 # Set up the font object
 font = pygame.font.SysFont("comicsansms", 20)
+message_font = pygame.font.SysFont("comicsansms", 70)
+
+# Display the main menu
+main_menu(screen)  # Call the main menu function
+
+
+def reset_game():
+    global score, balloons
+    score = 0
+    balloons.empty()  # Remove all balloons
+    Mechanism.bullets.empty()  # Remove all bullets
+    # Draw the level-up message
+    message = message_font.render("Level is up !!!", True, (255, 255, 255))
+    screen.blit(message, (300, 250))  # Center the message on the screen
+    pygame.display.flip()  # Update the display to show the message
+    pygame.time.delay(3000)
+    main_menu(screen)  # Call the main menu function
+
 
 # Game loop
 while True:
@@ -57,14 +73,24 @@ while True:
         for balloon in collisions:
             score += 1
 
+    # Check if score has reached 10
+    if score == 10:
+        level += 1
+        reset_game()
+
     # Draw everything
     # screen.fill((135, 206, 235))
     screen.blit(background, (0, 0))  # Draw the background image
     balloons.draw(screen)
     Mechanism.bullets.draw(screen)
     shooter.draw(screen)
+
     # Draw the score to the screen
     score_text = font.render(f'Score: {score}', True, (255, 255, 255))
     screen.blit(score_text, (900, 5))
+
+    # Draw the level to the screen
+    level_text = font.render(f'Level: {level}', True, (255, 255, 255))
+    screen.blit(level_text, (900, 30))
 
     pygame.display.flip()
